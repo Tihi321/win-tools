adde962d4c8d50077c4bb8147f62a79d1c2f3401
Tihomir Selak <tknox.dr@gmail.com>
2024-07-16 00:56:59 +0200

    WTL-02: Add scripts into backend

--- src-tauri/src/scripts/terminal.rs
+++ src-tauri/src/scripts/terminal.rs
@@ -63,7 +63,7 @@ pub fn start_script(script: &Script) -> io::Result<bool> {
 }
 
 pub fn stop_script(name: &str) -> io::Result<bool> {
-    let stop_service_command = format!("taskkill /IM {0}.bat /F", name);
+    let stop_service_command = format!("taskkill /IM {0} /F", name);
 
     let _ = execute_hidden_command(&stop_service_command);
 
